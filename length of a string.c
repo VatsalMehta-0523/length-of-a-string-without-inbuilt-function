e81d12a03900b0f2ca9209e0042f1d2a156095d7
VatsalMehta-0523 <168580559+VatsalMehta-0523@users.noreply.github.com>
2024-05-03 01:27:20 +0530

    Add files via upload

---
+++ length of a string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int len(char arr[]);                                 // function declaration
+
+int main(){
+    char arr[20];
+    printf("enter your string : ");
+    gets(arr);                                       // function calling
+
+    int n = len(arr);
+    printf("length of your string is %d",n);
+    return 0;
+}
+
+int len(char arr[]){                                 // function defination
+    int n= 0 ;                                       // counter variable
+    for (int i = 0 ; arr[i] ; i++){
+        n++;
+    }
+    return n;
+}
